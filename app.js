3bada21cbbe0ce9ca64fcaaf9b2c3276d619aa8d
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-01-23 23:08:49 +0000

    changes to mongoUpdate to allow for actions that should return more than a status code

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
+var bodyParser = require('body-parser');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -25,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
